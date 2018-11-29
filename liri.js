90526998a317924583076a507c899b78f7ac5300
savinaxu <jia_shijie@163.com>
2018-11-28 23:10:20 -0800

    motified some js.

--- liri.js
+++ liri.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const moment = require('moment');
 const request = require('request');
+const keys = require('./keys')
+const Spotify = require('node-spotify-api');
 const fs = require('fs');
+const spotify = new Spotify(keys.spotify);
 let userInput = process.argv.slice(3).join(" ");
 let search = process.argv[2]
 
@@ -10,11 +13,9 @@ let concertAPI = function(name) {
 
     request(queryURL, function (error, response, body) {
         if (error) {
-            console.log("\r\n\r\n\r\n");
-
+            console.log("=======================================================================");
             console.log("Sorry we can't get the data you want! Try something else.");
-      
-            console.log("\r\n\r\n\r\n");
+            console.log("=======================================================================");
       
             return;
         }
@@ -22,11 +23,9 @@ let concertAPI = function(name) {
         let result = JSON.parse(body)
 
         if (result.length === 0) {
-            console.log("\r\n\r\n\r\n");
-
+            console.log("=======================================================================");
             console.log("Sorry we can't get the data you want! Try something else.");
-      
-            console.log("\r\n\r\n\r\n");
+            console.log("=======================================================================");
       
             return;
         }
@@ -43,19 +42,38 @@ let concertAPI = function(name) {
             } else {
                 location = city + ", " + country
             }
-            console.log("\r\n\r\n\r\n");
-            console.log("Name of the venue: " + name);
-            console.log("Venue location: " + location);
-            console.log("Date of the Event: " + date);
+            console.log("=======================================================================");
+            console.log("\nName of the venue: " + name);
+            console.log("\nVenue location: " + location);
+            console.log("\nDate of the Event: " + date);
         })
-
-
     })
 }
 
-// let spotifyAPI = function(name) {
+let spotifyAPI = function(name) {
+    spotify.search({ type: 'track', query: name}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
 
-// }
+        let result = data.tracks.items
+
+        result.forEach(function(item) {
+            let artist = item.artists[0].name
+            let album = item.album.name
+            let preview = item.preview_url
+            if (!preview) {
+                preview = item.external_urls.spotify
+            }
+            console.log("=======================================================================");
+            console.log("\nArtist(s): " + artist)
+            console.log("\nThe song's name: " + name)
+            console.log("\nA preview link of the song from Spotify: " + preview)
+            console.log("\nThe album that the song is from: " + album)
+
+        })
+    });
+}
 
 let omdbAPI = function(name) {
     if (name.length === 0) {
@@ -65,11 +83,9 @@ let omdbAPI = function(name) {
 
     request(queryURL, function (error, response, body) {
         if (error) {
-            console.log("\r\n\r\n\r\n");
-
+            console.log("=======================================================================");
             console.log("Sorry we can't get the data you want! Try something else.");
-      
-            console.log("\r\n\r\n\r\n");
+            console.log("=======================================================================");
       
             return;
         }
@@ -85,31 +101,63 @@ let omdbAPI = function(name) {
         let plot = result.Plot
         let actors = result.Actors
 
-        console.log("\r\n\r\n\r\n");
-        console.log("Title: " + title);
-        console.log("Year: " + year);
-        console.log("IMDB Rating: " + rating);
-        console.log("Rotten Tomatoes Rating: " + tomato);
-        console.log("Country: " + country);
-        console.log("Language: " + language);
-        console.log("Plot: " + plot);
-        console.log("Actors in the movie: " + actors);
-        console.log("\r\n\r\n\r\n");
-        
+        console.log("=======================================================================");
+        console.log("\nTitle: " + title);
+        console.log("\nYear: " + year);
+        console.log("\nIMDB Rating: " + rating);
+        console.log("\nRotten Tomatoes Rating: " + tomato);
+        console.log("\nCountry: " + country);
+        console.log("\nLanguage: " + language);
+        console.log("\nPlot: " + plot);
+        console.log("\nActors in the movie: " + actors);
+        console.log("=======================================================================");
     });
 }
 
+let random = function() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
 
-switch (search) {
-    case 'concert-this':
-        concertAPI(userInput)
-        break;
+        let dataArr = data.split(",");
+        let input = dataArr[0]
+        let search = dataArr[1]
 
-    case 'movie-this':
-        omdbAPI(userInput)
-        break;
-
-    default:
-        console.log("error!")
+        commend(input, search)
+        // for (let i = 0; i < dataArr.length; i+2) {
+        //     if ( i % 2 === 0 && i + 1 < dataArr.length) {
+        //         input = dataArr[i]
+        //         search = dataArr[i+1]
+        //     }
+        // }
+    })
 }
+
+function commend(search, userInput) {
+    switch (search) {
+        case 'concert-this':
+            concertAPI(userInput)
+            break;
+    
+        case 'spotify-this-song':
+            spotifyAPI(userInput)
+            break;
+    
+        case 'movie-this':
+            omdbAPI(userInput)
+            break;
+    
+        case 'do-what-it-says':
+            random()
+            break;
+    
+        default:
+            console.log("error!")
+    }
+}
+
+commend(search, userInput)
+
+
 
